3a293d43016788aa8cf9c29189ab84cafb5649f2
seungjin kim <kim.sjin1221@gmail.com>
2023-10-31 00:41:32 -0700

    Converted the application from Vue2 to Vue3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue'
+import './assets/main.css';
 
-import './assets/main.css'
-
-new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+const app = createApp(App)
+app.mount('#app')
