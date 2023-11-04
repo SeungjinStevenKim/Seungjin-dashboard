034596e65657d0159fa99d22706d18ee2657f303
seungjin kim <kim.sjin1221@gmail.com>
2023-11-03 18:43:35 -0700

    Implemented Generic inputfield component

---
+++ src/components/baseComponents/InputField.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <label v-if="label" :for="id">
+      {{ label }}
+    </label>
+    <input
+      :id="id"
+      :type="type"
+      :value="modelValue"
+      @input="updateInput"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text",
+    }
+  },
+  methods: {
+    updateInput(event) {
+      this.$emit("filterUpdated", event.target.value);
+    }
+  }
+};
+</script>
