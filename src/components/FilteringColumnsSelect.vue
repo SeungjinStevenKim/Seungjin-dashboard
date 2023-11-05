2ab4061227438bc1073098533eaf26fd1119ab18
seungjin kim <kim.sjin1221@gmail.com>
2023-11-05 14:57:45 -0700

    unified code style for multiSelect component

--- src/components/FilteringColumnsSelect.vue
+++ src/components/FilteringColumnsSelect.vue
@@ -1,41 +1,38 @@
 <template>
-    <Multiselect
+  <Multiselect
       :options="options"
       mode="tags"
       :searchable="true"
       :create-option="true"
       placeholder="Select columns you want to search for"
-      @change="this.onChange"
-      />
+      @change="onChange"
+  />
 </template>
 
 <script>
-  import Multiselect from '@vueform/multiselect'
-  import { TABLE_COLUMNS } from '../constants';
-  import { useTableStore } from '../store/TableStore';
+import Multiselect from '@vueform/multiselect'
+import { TABLE_COLUMNS } from '../constants';
+import { useTableStore } from '../store/TableStore';
+import { ref } from 'vue';
 
-  export default {
-    components: {
-      Multiselect,
-    },
-    setup() {
-      const tableStore = useTableStore()
-  
-    return { tableStore }
+export default {
+  components: {
+    Multiselect,
   },
+  setup() {
+    const tableStore = useTableStore();
+    const options = ref(TABLE_COLUMNS);
 
+    const onChange = (columns) => {
+      tableStore.setFilteringSelectedColumns(columns);
+    };
 
-    data() {
-      return {
-        options: TABLE_COLUMNS
-      }
-    },
-    methods:{
-      onChange(columns) {
-        this.tableStore.setFilteringSelectedColumns(columns)
-      }
-    }
-  }
+    return {
+      options,
+      onChange,
+    };
+  },
+};
 </script>
 
 <style src="@vueform/multiselect/themes/default.css"></style>
